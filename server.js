e5a67a283c771dcfa4c005d3fc3bc28f75824c9d
insomniKat <ru3ykat@gmail.com>
2021-11-02 22:51:37 -0400

    get query setup

--- server.js
+++ server.js
@@ -3,15 +3,38 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+function filterByQuery(query, notesArray) {
+    let notesTitleArray = [];
+    let filteredResults = notesArray;
+    if (query.notesTitle) {
+        // save notesTitle as a dedicated array
+        // if notesTitle is a string, place it into a new array and save
+        if (typeof query.notesTitle === 'string') {
+            notesTitleArray = [query.notesTitle];
+        }
+    } else {
+        notesTitleArray = query.notesTitle;
+    }
+    // loop through each title in the notesTitle array
+    notesTitleArray.forEach(title => {
+        filteredResults = filteredResults.filter(
+            note => note.notesTitle.indexOf(title) !== -1
+        );
+    });
+    if (query.text) {
+        filteredResults = filteredResults.filter(note => note.text === query.text);
+    }
+    return filteredResults;
+}
 
 
 app.get('/api/notes', (req, res) => {
     console.log('Yup, it works!');
     let results = notes;
     if (req.query) {
-        results = res.json(notes);
+        results = filterByQuery(req.query, results);
     }
-    res.sendStatus(404);
+    res.json(results);
 });
 
 // used to create routes to add notes to db
