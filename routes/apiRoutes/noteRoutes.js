f5ea6e6bef56d917a966b5546764d33d97ffe8b0
insomniKat <ru3ykat@gmail.com>
2021-11-07 18:22:35 -0500

    refactor

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -10,17 +10,6 @@ router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
-// may be useless...uuid is too complicated to search for directly... 
-// likely better to search by title
-// app.get('/api/notes/:id', (req, res) => {
-//     const result = findById(req.params.id, notes);
-//     if (result) {
-//         res.json(result);
-//     } else {
-//         res.send(404);
-//     }
-// });
-
 // used to create routes to add notes to db
 router.post('/notes', (req, res) => {
   // if data in req.body is incorrect, return 400 error
